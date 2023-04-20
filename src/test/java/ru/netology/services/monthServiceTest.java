c93f86b2a157e0bc513762532bb1cda785d008f6
MaxFunk <funk-masik@yandex.ru>
2023-04-20 12:59:34 +0300

    inicial commit

---
+++ src/test/java/ru/netology/services/monthServiceTest.java
@@ -0,0 +1,20 @@
+package ru.netology.services;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class monthServiceTest {
+    @Test
+    void Test() {
+        monthSrvice service = new monthSrvice();
+
+        int income = 10_000;
+        int expenses = 3_000;
+        int threshold = 20_000;
+
+        int actual = service.calculate(income, expenses, threshold);
+        int expected = 3;
+        Assertions.assertEquals(actual, expected);
+
+    }
+}
